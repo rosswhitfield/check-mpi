1102da3e4f46ffeb430deaab690c8ef95894da1c
Ross Whitfield <ross.whitfield@gmail.com>
2021-11-17 11:17:25 -0500

    Change affinity formatting

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,42 @@ pub fn get_affinty() -> String {
 
     let size = unsafe { libc::sysconf(libc::_SC_NPROCESSORS_ONLN) } as libc::size_t;
 
-    let mut cpu_list: Vec<String> = Vec::new();
-    for i in 0..size {
+    let mut cpu_list = String::new();
+
+    let mut i = 0;
+    loop {
         if unsafe { libc::CPU_ISSET(i, &mask) } {
-            cpu_list.push(i.to_string());
+            let mut run = 0;
+            let mut j = i + 1;
+            loop {
+                if unsafe { libc::CPU_ISSET(j, &mask) } {
+                    run += 1;
+                } else {
+                    break;
+                }
+                j += 1;
+                if j == size {
+                    break;
+                }
+            }
+            if !cpu_list.is_empty() {
+                cpu_list.push(',');
+            }
+            if run == 0 {
+                cpu_list.push_str(&format!("{}", i));
+            } else if run == 1 {
+                cpu_list.push_str(&format!("{},{}", i, i + 1));
+                i += 1;
+            } else {
+                cpu_list.push_str(&format!("{}-{}", i, i + run));
+                i += run;
+            }
+        }
+        i += 1;
+        if i == size {
+            break;
         }
     }
 
-    cpu_list.join(",")
+    cpu_list
 }
